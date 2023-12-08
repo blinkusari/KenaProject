778d10ec34ffa8bce86d04e42517430661e3b6b1
altinmullaidrizi <altin.mullaidrizi1@gmail.com>
2023-12-08 16:12:38 +0100

    changes

---
+++ wp-content/uploads/custom-css-js/loading.js
@@ -0,0 +1,10 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+/* Default comment here */ 
+
+jQuery(document).ready(function( $ ){
+// //      setTimeout(function() {
+        $('.loader-wrapper').fadeOut();
+// //     }, 2000);
+});</script>
+<!-- end Simple Custom CSS and JS -->
